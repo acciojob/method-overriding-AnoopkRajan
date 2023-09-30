cfa9536fff6f86326c9bfcc70515d7eaca2bc2e7
Anoop Rajan <anoopkrajan7@gmail.com>
2023-09-30 13:46:30 +0530

    assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,23 +3,23 @@ package com.driver;
 public class Main {
      public static class A {
 
-        public  String fun()
+        public  String meth()
         {
             return "Invoking method from class A";
         }
     }
 
     public static class B extends A {
-        public  String fun()
+        public  String meth()
         {
             return "Method is overridden in Extendend class B";
         }
     }
     public static void main(String[] args) {
-        B obj = new B();
-        obj.fun();
-
         B obj1 = new B();
-        obj1.fun();
+        obj1.meth();
+
+        B obj2 = new B();
+        obj2.meth();
     }
 }
